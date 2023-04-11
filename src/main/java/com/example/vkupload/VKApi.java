bbb4d4c684ba5a4faf5b56d7320e6ee49e27aa27
JusteRYT <tohappyoneguy@gmail.com>
2023-04-11 23:28:50 +0300

    dsa

--- src/main/java/com/example/vkupload/VKApi.java
+++ src/main/java/com/example/vkupload/VKApi.java
@@ -1,28 +1,24 @@
 package com.example.vkupload;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
+import com.vk.api.sdk.actions.OAuth;
+import com.vk.api.sdk.client.VkApiClient;
+import com.vk.api.sdk.client.actors.UserActor;
+import com.vk.api.sdk.exceptions.ApiException;
+import com.vk.api.sdk.exceptions.ClientException;
+import com.vk.api.sdk.httpclient.HttpTransportClient;
+import com.vk.api.sdk.objects.UserAuthResponse;
 
-import javax.xml.bind.DatatypeConverter;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.List;
+
+import static com.sun.javafx.scene.control.skin.FXVK.vk;
 
 public class VKApi {
     private int appId;
     private String appSecret;
     private String login;
     private String password;
+    private String redirectUri = "https://oauth.vk.com/blank.html";
+    VkApiClient vk = new VkApiClient(HttpTransportClient.getInstance());
 
     public VKApi(int appId, String appSecret, String login, String password) {
         this.appId = appId;
@@ -31,72 +27,37 @@ public class VKApi {
         this.password = password;
     }
 
-    public String getAccessToken() throws ClientProtocolException, IOException, NoSuchAlgorithmException {
-        // Получаем адрес страницы авторизации
-        String authUrl = "https://oauth.vk.com/authorize" +
-                "?client_id=" + appId +
-                "&redirect_uri=https://oauth.vk.com/blank.html" +
-                "&display=mobile" +
-                "&scope=friends,photos,audio,video,docs,notes,pages,status,wall,groups,messages,offline" +
-                "&response_type=token";
+    public String getAuthorizationCode() throws IOException {
+        // Параметры авторизации
+        String clientId = "your_client_id";
+        String redirectUri = "https://oauth.vk.com/blank.html";
+        String scope = "friends,photos,audio,video,docs,notes,pages,status,wall,groups,messages,offline";
+        String responseType = "code";
 
-        // Получаем страницу авторизации и извлекаем параметр hash
-        HttpClient httpClient = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost(authUrl);
-        HttpResponse httpResponse = httpClient.execute(httpPost);
-        String content = org.apache.http.util.EntityUtils.toString(httpResponse.getEntity());
-        String hash = extractHash(content);
+        // Получение URL для запроса авторизации
+        String authorizationUri = vk.oAuth()
+                .authorizationCodeFlow(clientId, redirectUri)
+                .scope(scope)
+                .responseType(responseType)
+                .state("secret_state_string")
+                .build();
 
-        // Формируем данные для отправки POST запроса на получение access_token
-        String data = "client_id=" + appId +
-                "&client_secret=" + appSecret +
-                "&grant_type=password" +
-                "&username=" + URLEncoder.encode(login, "UTF-8") +
-                "&password=" + URLEncoder.encode(password, "UTF-8") +
-                "&scope=friends,photos,audio,video,docs,notes,pages,status,wall,groups,messages,offline" +
-                "&response_type=token" +
-                "&hash=" + hash;
+        // Предполагается, что эта ссылка будет открыта пользователем в браузере, и пользователь разрешит доступ
+        System.out.println("Please open the following URL in your browser and authorize the application:");
+        System.out.println(authorizationUri);
 
-        // Отправляем POST запрос и получаем access_token
-        httpPost = new HttpPost("https://oauth.vk.com/token");
-        List<NameValuePair> nameValuePairs = new ArrayList<>();
-        String[] params = data.split("&");
-        for (String param : params) {
-            String[] parts = param.split("=");
-            nameValuePairs.add(new BasicNameValuePair(parts[0], parts[1]));
-        }
-        httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-        httpResponse = httpClient.execute(httpPost);
-        content = org.apache.http.util.EntityUtils.toString(httpResponse.getEntity());
-        String accessToken = extractAccessToken(content);
+        // Получение authorization code вручную
+        System.out.println("Please enter the code you received after authorizing the application:");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String code = reader.readLine();
 
-        return accessToken;
+        return code;
     }
 
-    private String extractHash(String content) throws UnsupportedEncodingException {
-        int startIndex = content.indexOf("hash=") + 5;
-        int endIndex = content.indexOf("\"", startIndex);
-        String hash = content.substring(startIndex, endIndex);
-        hash = URLEncoder.encode(hash, "UTF-8");
-        return hash;
-    }
+    public String getAccessToken(String code) throws ClientException, ApiException {
+        UserAuthResponse authResponse = vk.oAuth()
+                .userAuthorizationCodeFlow(appId, appSecret, redirectUri, code)
+                .execute();
 
-    private String extractAccessToken(String content) throws NoSuchAlgorithmException {
-        int startIndex = content.indexOf("access_token=") + 17;
-        int endIndex = content.indexOf("&", startIndex);
-        String accessToken = content.substring(startIndex, endIndex);
-        // Проверяем корректность полученного access_token с помощью подсчета MD5 хэша
-        MessageDigest md = MessageDigest.getInstance("MD5");
-        String checkString = appId + "_" + accessToken + "_" + appSecret;
-        byte[] digest = md.digest(checkString.getBytes());
-        String expectedHash = DatatypeConverter.printHexBinary(digest).toLowerCase();
-        startIndex = content.indexOf("email=") + 6;
-        endIndex = content.indexOf("&", startIndex);
-        String email = content.substring(startIndex, endIndex);
-        if (!expectedHash.equals(email)) {
-            throw new NoSuchAlgorithmException("Invalid access_token");
-        }
-
-        return accessToken;
-    }
+        return authResponse.getAccessToken();
 }
